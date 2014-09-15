96c61a43e8ab09ad5ea90e995bf54626ff706a05
Gary Spagnoli <gary.spagnoli@gmail.com>
2014-09-15 12:43:10 -0400

    removed the less file. switched over to sass

---
+++ db/migrate/20140915163648_add_to_users_latitude_longitude.rb
@@ -0,0 +1,6 @@
+class AddToUsersLatitudeLongitude < ActiveRecord::Migration
+  def change
+  	add_column :users, :latitude, :float
+    add_column :users, :longitude, :float
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915033447) do
+ActiveRecord::Schema.define(version: 20140915163648) do
 
   create_table "games", force: true do |t|
     t.datetime "time"
@@ -49,8 +49,8 @@ ActiveRecord::Schema.define(version: 20140915033447) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-    t.float    "longitude"
     t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
