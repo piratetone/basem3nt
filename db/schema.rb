ece013384cc69dacbbcd1a5364fdb25f6477d960
Gary Spagnoli <gary.spagnoli@gmail.com>
2014-09-15 12:58:38 -0400

    created invites controller, model and database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915163648) do
+ActiveRecord::Schema.define(version: 20140915165626) do
 
   create_table "games", force: true do |t|
     t.datetime "time"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20140915163648) do
     t.datetime "updated_at"
     t.integer  "uid"
     t.integer  "user_id"
+  end
+
+  create_table "invites", force: true do |t|
+    t.integer  "attendee_id"
+    t.integer  "attended_event_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
